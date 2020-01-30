07548a1306531a7af8b713970600718f6053779c
Donald Lee Talbert <donleetalbert@gmail.com>
2020-01-29 23:57:29 -0500

    Adding meow_meow and app_data comments.

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 msg = "Donald Lee Talbert & YingJing Yuan Talbert wants to be included in the GitHub Arctic archive!"
+msg = msg + "Oh Meow Meow too!"
+# TODO: Hey, check out our app_data imgs!
+
 print(msg)
 
 exit()
